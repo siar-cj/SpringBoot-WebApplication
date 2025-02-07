58e525bc427ecc07c8ce70b9832b5892d2bb23ea
Siva CS Javvadhi <118528602+siar-cj@users.noreply.github.com>
2025-02-07 16:21:06 +0530

    Update StartApplication.java

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "This is a SpringBoot Static Web Application");
-        model.addAttribute("msg", "Application Is Deployed To Kuberneets");
+        model.addAttribute("title", "Siva Chandrasekhar J");
+        model.addAttribute("msg", "07-02-2025 | SpringBoot Application Deployment - Done");
         return "index";
     }
 
